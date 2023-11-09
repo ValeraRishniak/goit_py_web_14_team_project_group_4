f4923041fc28d359e809a868659b49f922299752
VladyslavBon <vladislav.bondarenko96@gmail.com>
2023-11-09 17:28:39 +0200

    Update db.py

--- app/database/db.py
+++ app/database/db.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import sessionmaker
 from app.conf.config import settings
 
 
-SQLALCHEMY_DATABASE_URL = settings.sqlalchemy_database_url
-engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+DATABASE_URL = settings.db_url
+engine = create_engine(DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
