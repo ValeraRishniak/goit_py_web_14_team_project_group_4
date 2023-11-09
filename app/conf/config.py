ca1ae10797bee391de679f12ae1e0213af125e14
VladyslavBon <vladislav.bondarenko96@gmail.com>
2023-11-09 18:57:03 +0200

    upd

--- app/conf/config.py
+++ app/conf/config.py
@@ -3,21 +3,6 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     db_url: str
-    secret_key: str
-    algorithm: str
-    mail_username: str
-    mail_password: str
-    mail_from: str
-    mail_port: int
-    mail_server: str
-    mail_from_name: str
-    redis_host: str
-    redis_port: int
-    redis_password: str
-    cloudinary_name: str
-    cloudinary_api_key: str
-    cloudinary_api_secret: str
-    cloudinary_folder: str
 
     class Config:
         env_file = ".env"
