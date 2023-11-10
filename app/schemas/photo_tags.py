51416119bd03e51ab9ae92dec61c1fbe9b6abbd6
Topsya <topsya1986@gmail.com>
2023-11-10 18:30:24 +0200

    fo photo new update

--- app/schemas/photo_tags.py
+++ app/schemas/photo_tags.py
@@ -2,24 +2,24 @@ from datetime import date, datetime
 from typing import List, Optional
 from pydantic import BaseModel, Field, EmailStr, HttpUrl
 
-
-class TagPhoto(BaseModel):
-    name: str   = Field(max_length=25)
+from app.schemas.tags import ImageTagResponse
+# class TagPhoto(BaseModel):
+#     name: str   = Field(max_length=25)
     
-class TagPhotoResponse(TagPhoto):
-    id: int
+# class TagPhotoResponse(TagPhoto):
+#     id: int
 
-    class Config:
-        from_attributes = True
+#     class Config:
+#         from_attributes = True
 
 class PhotoBase(BaseModel):
      url: HttpUrl
      name: str
 
-class PhotoMoedels(PhotoBase):
+class PhotoModels(PhotoBase):
     description: str | None = None
     photo: PhotoBase | None = None
-    tags: List[TagPhotoResponse]
+    tags: List[ImageTagResponse]
     id  : int
      
     class Config:
