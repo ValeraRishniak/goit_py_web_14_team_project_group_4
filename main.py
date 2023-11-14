7ceb5f729c1fb98b33eaf69fe1b703eb1a2f124f
Topsya <topsya1986@gmail.com>
2023-11-14 23:15:13 +0200

    подправили маин

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 
-from app.routes import tags
+from app.routes import tags, comment, photo, users
 
 app = FastAPI()
 
@@ -12,6 +12,9 @@ async def root():
 
 
 app.include_router(tags.router, prefix="/api")
+app.include_router(comment.router, prefix="/api")
+app.include_router(photo.router, prefix="/api")
+app.include_router(users.router, prefix="/api")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, reload=True)
