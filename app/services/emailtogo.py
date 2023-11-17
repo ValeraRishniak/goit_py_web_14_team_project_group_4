df56b73440a01ba6accf9bca1f484e50ce64ba08
Topsya <topsya1986@gmail.com>
2023-11-17 09:09:06 +0200

    add authorization from send email

---
+++ app/services/emailtogo.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+from fastapi_mail import FastMail, MessageSchema, ConnectionConfig, MessageType
+from fastapi_mail.errors import ConnectionErrors
+from pydantic import EmailStr
+
+from app.conf.config import settings
+from app.services.auth import auth_service
+
+conf = ConnectionConfig(
+    MAIL_USERNAME=settings.mail_username,
+    MAIL_PASSWORD=settings.mail_password,
+    MAIL_FROM=  settings.mail_from ,
+    MAIL_PORT=settings.mail_port,
+    MAIL_SERVER=settings.mail_server,
+    MAIL_FROM_NAME="Team work for python web",
+    MAIL_STARTTLS=False,
+    MAIL_SSL_TLS=True,
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True,
+    TEMPLATE_FOLDER=Path(__file__).parent / 'templates',
+)
+
+
+async def send_email(email: EmailStr, username: str, host: str):
+    """
+    The send_email function sends an email to the user with a link to confirm their email address.
+       
+    """
+    try:
+        token_verification = auth_service.create_email_token({"sub": email})
+        message = MessageSchema(
+            subject="Confirm your email ",
+            recipients=[email],
+            template_body={"host": host, "username": username, "token": token_verification},
+            subtype=MessageType.html
+        )
+
+        fm = FastMail(conf)
+        await fm.send_message(message, template_name="email_template.html")
+    except ConnectionErrors as err:
+        print(err)
