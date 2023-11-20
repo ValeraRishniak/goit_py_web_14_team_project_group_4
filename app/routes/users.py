1921e6724fd0bc18da951d7958d2b3d7a2dc6ee7
ValeraRishniak <128408270+ValeraRishniak@users.noreply.github.com>
2023-11-20 09:53:27 +0200

    Update users.py

--- app/routes/users.py
+++ app/routes/users.py
@@ -28,8 +28,8 @@ async def update_avatar_user(file: UploadFile = File(), current_user: User = Dep
     config_cloudinary()
 
     r = cloudinary.uploader.upload(
-        file.file, public_id=f'PhotoSHAKE/{current_user.username}', overwrite=True)
-    src_url = cloudinary.CloudinaryImage(f'PhotoSHAKE/{current_user.username}')\
+        file.file, public_id=f'PhotoSHAKE/{current_user.username}/{current_user.id}', overwrite=True)
+    src_url = cloudinary.CloudinaryImage(f'PhotoSHAKE/{current_user.username}/{current_user.id}')\
                         .build_url(width=250, height=250, crop='fill', version=r.get('version'))
     user = await repository_users.update_avatar(current_user.email, src_url, db)
     return user
